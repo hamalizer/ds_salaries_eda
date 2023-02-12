0aab669b1ac2d1c1df1e1ec7088e35c7c1852aeb
DA OG <og@constructive.consulting>
2023-02-11 18:14:48 -0900

    setup4

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+
+st.header('Employee Attrition: A short story.')
+
+st.write('It is not a functional application yet. Under construction.') 
